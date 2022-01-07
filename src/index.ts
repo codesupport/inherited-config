69b1f24fe82a7cfc383ce89fb4d45fbd265bfcde
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2022-01-07 19:19:04 +0000

    Create simple config system.

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import * as process from "process";
+import * as path from "path";
+import * as fs from "fs";
+
+interface Options {
+	path: string;
+	log: boolean;
+}
+
+class InheritedConfig {
+	private readonly config: Record<string, any> = {};
+	private readonly options: Options = {
+		log: false,
+		path: ""
+	};
+
+	require(path: string): any {
+		return require(path);
+	}
+
+	private log(message: string): void {
+		if (this.options.log) {
+			console.debug(`[@codesupport/inherited-config DEBUG] ${message}`);
+		}
+	}
+
+	constructor(options?: Partial<Options>) {
+		const root = path.resolve(process.cwd(), options?.path ?? "");
+		const productionConfig = path.resolve(root, "config.json");
+
+		this.options = { ...this.options, ...options };
+
+		if (fs.existsSync(productionConfig)) {
+			this.config = this.require(productionConfig);
+
+			const env = process.env.NODE_ENV;
+			const envConfig = path.resolve(root, `config.${env?.toLowerCase()}.json`);
+
+			if (env && fs.existsSync(envConfig)) {
+				this.config = {
+					...this.config,
+					...this.require(envConfig)
+				};
+			}
+		} else {
+			this.log(`Failed to initialise Inherited Config: no file at location '${productionConfig}'.`);
+		}
+	}
+
+	getValue<T>(key: string): T | undefined {
+		return this.config[key];
+	}
+}
+
+export default InheritedConfig;
